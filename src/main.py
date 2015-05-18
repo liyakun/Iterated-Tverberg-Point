485c6e227b9e93a3aeafa46ceb660f1b8899eaa7
liyakun <liyakun127@hotmail.com>
2015-05-18 03:23:21 +0000

    get linear model and plot

--- src/main.py
+++ src/main.py
@@ -1,20 +1,32 @@
 #! /usr/bin/pythonw
 #This file contains the main function
-
+import csv
 import Regression
 import Data
+import Plot
+
+weights = []
+weights_all = []
 
 #filter data with unknown features && create dummy mat matrix
 myData = Data.Data()
 myData.loadDataSet()
 myData.filterDataSet()
 myData.convertAttrToMatrix()
-matrix_data, teacher_label = myData.returnPartDataMatrix(1, 500)
+myData.splitToTrainAndTest()
 
 #regression algorithm
 myRegression = Regression.Regression()
-weights = myRegression.gradAscent(matrix_data, teacher_label, 300)
+for i in range(0, 2):
+  print  i,"th training."
+  weights.append(myRegression.gradAscent(myData.train_matrix, myData.train_class_list, myData.getRandomIndexList(1500), 300))
+  weights_all.append(myRegression.weights_all)
 
+#write trained weights to file
+with open("../resources/output_weights.csv", "wb") as f:
+    writer = csv.writer(f)
+    writer.writerows(weights)
 
-
-
+#plot weights convergence from regression
+myPlot = Plot.Plot()
+myPlot.plot(weights_all)
