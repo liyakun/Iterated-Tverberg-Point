c6c893b954ca1d86a064733d288d2d53ffb0706f
liyakun <liyakun127@hotmail.com>
2015-05-18 03:22:15 +0000

    add get weights_all from optimization

--- src/Regression.py
+++ src/Regression.py
@@ -1,7 +1,7 @@
 #! /usr/bin/pythonw
 #This file contains the logistic regression algorithm
 
-from numpy import *
+import numpy as np
 import Optimization
 
 class Regression:
@@ -9,18 +9,21 @@ class Regression:
     logistic regression algorithm
     """
 
+    weights_all = []
+
     def __init__(self):
         pass
 
     #calculate the sigmoid function
     def sigmoid(self, inX):
-        return 1.0/(1+exp(-inX))
+        return 1.0/(1+np.exp(-inX))
 
     #train a logistic regression model using some optional optimize algorithm
-    def gradAscent(self, dataMatrix, classLabels, numIteration):
+    def gradAscent(self, trainMatrix, classLabels, dataIndexMatrix, numIteration):
         regOpt = Optimization.Optimization()
-        return regOpt.gradAscent(dataMatrix, classLabels, numIteration, self)
-
+        weights = regOpt.gradAscent(trainMatrix, classLabels, dataIndexMatrix, numIteration, self)
+        self.weights_all = regOpt.weights_all
+        return weights
 
 
 
