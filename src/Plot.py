795ebef5489c405ebdff9ab4882e5a8b63adf6e8
liyakun <liyakun127@hotmail.com>
2015-05-18 03:24:40 +0000

    add plot class

---
+++ src/Plot.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import matplotlib.backends.backend_pdf as pltpage
+
+
+class Plot:
+
+    def __init__(self):
+        pass
+
+    def plot(self, weights_all):
+        size = len(weights_all)
+        with pltpage.PdfPages("../resources/pic/weights.pdf") as pdf:
+            for i in xrange(1, size):
+                fig = plt.figure(figsize=(8.27, 11.69), dpi=100)
+                plt.plot(weights_all[i])
+                pdf.savefig(fig)
+                plt.close()
+        pdf.close()
+
