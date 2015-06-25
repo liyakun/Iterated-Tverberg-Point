f07d40f3d1f8a69028a5c96616c4240a7ca98c9d
liyakun <liyakun127@gmail.com>
2015-06-26 00:23:39 +0200

    add flexible fold of cross-validation

--- src/main.py
+++ src/main.py
@@ -1,29 +1,18 @@
 """
 This file contains the main function
 """
-from src.lib import test
-from src.model import haberman
 from src.model import skin
-from src.lib import plot
-
-""" run haberman test """
-# my_haber = haberman.Haberman()
-# weights, coefficients, mean_point = my_haber.run_sklearn()
-# my_test = test.Test()
-# my_test.perform_test(my_haber.X, my_haber.Y, weights, coefficients, mean_point, "../resources/haberman/error.txt")
 
 """ run skin test """
 my_skin = skin.Skin()
-my_skin.run_skin(1000, 2000)
-# my_test = test.Test()
-# weights, coefficients, mean_point = my_skin.run_sklean()
-# my_test.perform_test(my_skin.X, my_skin.Y, weights, coefficients, mean_point, "../resources/skin/error.txt")
+# number_of_training, number_of_training_instances, number_of_fold, percentage_of_training(e.g. 7) 70%
+my_skin.run_skin_n_fold(1000, 2000, 2, 7)
 
 """ run bank test """
 # my_bank = bank.Bank()
 # my_bank.runbank(1000, 2000)
 
 """ test plot error """
-my_plot = plot.Plot()
-my_plot.plot_error("../resources/skin/result/error_random.txt")
-my_plot.plot_error("../resources/skin/result/error_equal.txt")
+# my_plot = plot.Plot()
+# my_plot.plot_error("../resources/skin/result/error_random.txt")
+# my_plot.plot_error("../resources/skin/result/error_equal.txt")
--- src/model/skin.py
+++ src/model/skin.py
@@ -5,29 +5,36 @@ https://archive.ics.uci.edu/ml/datasets/Skin+Segmentation
 
 from src.lib import data, itertver, regression, test
 
-
 class Skin:
     # prepare data
     my_data = data.Data()
     my_data.load_skin_data()
-    X, Y = my_data.get_skin_data()
-    X_train, Y_train, X_test, Y_test = my_data.split_to_train_and_test(X, Y, 7)
 
-    def run_skin(self, number_of_training, number_of_training_instances):
+    # each run will get different subset from X, Y, as the matrix are shuffled before they are split
+    def get_skin_data(self, percent_of_train):
+        x, y = self.my_data.get_skin_data()
+        x_train, y_train, x_test, y_test = self.my_data.split_to_train_and_test(x, y, percent_of_train)
+        return x_train, y_train, x_test, y_test, x, y
+
+    def run_skin_one_fold(self, number_of_training, number_of_training_instances, fold, percent_of_train):
+        # get skin data
+        x_train, y_train, x_test, y_test, x, y = self.get_skin_data(percent_of_train)
+
         # get training weights
-        weights_random = regression.Regression().gradient_descent_random_general(self.X_train, self.Y_train,
+        weights_random = regression.Regression().gradient_descent_random_general(x_train, y_train,
                                                                                  number_of_training,
                                                                                  number_of_training_instances)
 
-        weights_all = regression.Regression().gradient_descent_all(self.X_train, self.Y_train)
+        weights_all = regression.Regression().gradient_descent_all(x_train, y_train)
 
-        data_set, label = self.my_data.get_disjoint_subset_data(2000, self.X, self.Y)
+        data_set, label = self.my_data.get_disjoint_subset_data(2000, x, y)
+
         weights_equal = regression.Regression().gradient_descent_equal(data_set, label)
 
         # write trained weights to file
-        self.my_data.write_to_csv_file("../resources/skin/result/output_weights_random.csv", weights_random)
-        self.my_data.write_to_csv_file("../resources/skin/result/output_weights_equal.csv", weights_equal)
-        self.my_data.write_to_csv_file("../resources/skin/result/output_weights_all.csv", weights_all)
+        self.my_data.write_to_csv_file("../resources/skin/result/output_weights_random"+str(fold)+".csv", weights_random)
+        self.my_data.write_to_csv_file("../resources/skin/result/output_weights_equal"+str(fold)+".csv", weights_equal)
+        self.my_data.write_to_csv_file("../resources/skin/result/output_weights_all"+str(fold)+".csv", weights_all)
 
         # get center point
         my_center_point = itertver.IteratedTverberg()
@@ -35,8 +42,15 @@ class Skin:
         center_point_equal, average_point_euqal = my_center_point.get_center_and_average_point(weights_equal)
 
         # testing phase
-        test.Test().perform_test(self.X_test, self.Y_test, weights_random, center_point_random,
-                                 average_point_random, weights_all, "../resources/skin/result/error_random.txt")
+        test.Test().perform_test(x_test, y_test, weights_random, center_point_random,
+                                 average_point_random, weights_all, "../resources/skin/result/"+str(fold) +
+                                 "error_random.txt")
 
-        test.Test().perform_test(self.X_test, self.Y_test, weights_equal, center_point_equal,
-                                 average_point_euqal, weights_all, "../resources/skin/result/error_equal.txt")
+        test.Test().perform_test(x_test, y_test, weights_equal, center_point_equal,
+                                 average_point_euqal, weights_all, "../resources/skin/result/"+str(fold) +
+                                 "error_equal.txt")
+
+    def run_skin_n_fold(self, number_of_training, number_of_training_instances, number_of_fold, percent_of_train):
+        for i in range(number_of_fold):
+            print str(i)+"th fold cross validation."
+            self.run_skin_one_fold(number_of_training, number_of_training_instances, i, percent_of_train)
